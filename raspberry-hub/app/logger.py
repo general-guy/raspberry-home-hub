b924bb3d43d0555b625a21fe7b1ac78277f7bdc3
Geraldo Machado Filho <geraldo18@gmail.com>
2026-05-12 15:19:05 -0300

    Stabilize MQTT operational lifecycle

--- raspberry-hub/app/logger.py
+++ raspberry-hub/app/logger.py
@@ -3,4 +3,8 @@ from datetime import datetime
 
 def log(message: str):
     now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    print(f"[{now}] {message}")
+
+    print(
+        f"[{now}] {message}",
+        flush=True
+    )
